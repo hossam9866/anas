90f8a3615fd7d34c493c5a958dcb6bc876796b6f
hossam9866 <77072140+hossam9866@users.noreply.github.com>
2022-11-23 17:19:39 +0200

    gg

--- js/function.js
+++ js/function.js
@@ -12,9 +12,27 @@ window.onload = () => {
     if (event.target.value < 0) {
       event.target.value = 0;
     }
+    if (event.target.value > 4416) {
+      event.target.value = 4416;
+    }
   });
 
-  document.querySelector("button").onclick = (event) => {
+  window.addEventListener("click", (event) => {
+    if (event.target.getAttribute("id") == "restore") {
+      document.getElementById("not-ok").style.display = "none";
+      document.getElementById("ok").style.display = "none";
+      document.getElementById("content").style.display = "flex";
+      document.getElementById("gb").value = "";
+      document.getElementById("sub-years").value = 0.9;
+      document.querySelector(".slider-show").innerText = 0.9;
+      document.getElementById("left-years").value = 0;
+      document.getElementById("times-fail").value = 0;
+      document.querySelectorAll(".slider-show")[1].innerText = 0;
+      document.querySelectorAll(".slider-show")[2].innerText = 0;
+    }
+  });
+
+  document.querySelector("#submit").onclick = (event) => {
     event.preventDefault();
     let tv = document.getElementById("tv-sub");
     let gb = document.getElementById("gb");
@@ -23,7 +41,7 @@ window.onload = () => {
     let timesFail = document.getElementById("times-fail");
     console.log(
       tv.checked,
-      gb.value,
+      gb.value ? gb.value : "0",
       subYears.value,
       leftYears.value,
       timesFail.value
@@ -32,13 +50,20 @@ window.onload = () => {
       tv.checked ? 1 : 0
     }&subscription_age=${subYears.value}&remaining_contract=${
       leftYears.value
-    }&service_failure_count=${timesFail.value}&download_avg=${gb.value}`;
+    }&service_failure_count=${timesFail.value}&download_avg=${
+      gb.value ? gb.value : "0"
+    }`;
     console.log(url);
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", url);
-    xhttp.send();
-    xhttp.onload((res) => {
-      console.log(res);
-    });
+    fetch(url)
+      .then((res) => res.text())
+      .then((data) => {
+        if (data == 0) {
+          document.getElementById("content").style.display = "none";
+          document.getElementById("ok").style.display = "block";
+        } else {
+          document.getElementById("content").style.display = "none";
+          document.getElementById("not-ok").style.display = "block";
+        }
+      });
   };
 };
